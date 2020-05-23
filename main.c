8c5fdf0759c104fc59c62831b82167942bec96b6
JP Ferreira <jotepauder01@gmail.com>
2020-05-23 16:03:23 -0300

    Aprendendo c/c++

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main(void){
 	printf("HelloWorld");
 	printf("Testando o compilador de C e C++ no Windows");
-	
-	
+	printf("Cadê a motivação para estudar programação e os outros assuntos?");
 }
+
