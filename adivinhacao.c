dcf31f752cded213361c657f76359aca56fe5963
JP Ferreira <jotepauder01@gmail.com>
2020-10-13 19:46:04 -0300

    Organizando a estrutura

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,29 +1,8 @@
-#include <stdio.h>
-#include <locale.h>
-#include <windows.h>
-
-int main() { 
+int main(){
     
-     
-    UINT CPAGE_UTF8 = 65001;
-    UINT CPAGE_DEFAULT = GetConsoleOutputCP();
-    
-    
-     SetConsoleOutputCP(CPAGE_UTF8);
-    // imprime o cabeçalho do nosso jogo
-    printf("**************************************\n");
-    printf("* Bem vindo ao nosso jogo de adivinhação! *\n");
-    printf("**************************************\n");
+    printf("Testando\n");
+    printf("Testando2 \n");
+    printf("Olá caraio,estou aqui de novo\n");
+    printf("Relembradando o c\n");
 
-    int numerosecreto = 42;
-    int chute;
-
-    printf("Qual é o seu chute? ");
-    scanf("%d",&chute);
-    printf("Seu chute foi %d\n", chute);
-
-    SetConsoleOutputCP(CPAGE_DEFAULT);
-    
-    // declaração de uma variavel inteiro -> %d
-    //printf(" O número %d é o secreto. Não conta pra ninguém!\n", numerosecreto);
 }
