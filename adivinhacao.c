b8b6eb5b80fc4950194450157471c7358929de64
JP Ferreira <jotepauder01@gmail.com>
2020-10-20 11:45:09 -0300

    Usando WSL2

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,8 +1,14 @@
+#include <stdio.h>
+
 int main(){
     
     printf("Testando\n");
     printf("Testando2 \n");
     printf("Olá caraio,estou aqui de novo\n");
     printf("Relembradando o c\n");
+    printf("Bora? Então?\n");
+    printf("Olá,bora tc?");
+    
+
 
 }
