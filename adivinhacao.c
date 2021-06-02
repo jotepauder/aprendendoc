d796790bd5e7803bde035f6df58f7503bda0d6e5
JP Ferreira <jotepauder01@gmail.com>
2021-06-01 23:36:30 -0300

    Update no curso

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
+int main() {
     
-    printf("Testando\n");
-    printf("Testando2 \n");
-    printf("Olá caraio,estou aqui de novo\n");
-    printf("Relembradando o c\n");
-    printf("Bora? Então?\n");
-    printf("Olá,bora tc?");
-    
+    // imprime cabecalho do nosso jogo
+    printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
 
+    int numerosecreto = 42;
 
+    int chute;
+
+    printf("Qual é o seu chute ? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d" , chute);
+
+     
 }
+
+//printf("O número %d  é o secreto. Não conta pra ninguém!\n", numerosecreto);
