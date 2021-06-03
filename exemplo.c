0cab7d14cffc9ada82eedd0d5bf42708f476a103
JP Ferreira <jotepauder01@gmail.com>
2021-06-02 22:43:48 -0300

    Update curso C

---
+++ exemplo.c
@@ -0,0 +1,27 @@
+/*int numero = 15;
+if (numero > 20) {
+  printf("numero grande");
+} else {
+  printf("numero pequeno");
+}
+*/
+
+// Exemplo for if and else 
+
+
+//Exemplo 2 
+
+/*int chute = 0;
+int numerosecreto = 42;
+int acertou = chute == numerosecreto;
+if(acertou) {
+    printf("Parabéns! Você acertou!\n");
+} else {
+    if(chute > numerosecreto) {
+        printf("Seu chute foi maior do que o número secreto!\n");
+    } 
+    if(chute < numerosecreto) {
+        printf("Seu chute foi menor do que o número secreto!\n");
+    }
+}
+*/
