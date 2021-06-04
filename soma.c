3c8d5f353b3c283b6f7c49998efe0f88c81d803a
JP Ferreira <jotepauder01@gmail.com>
2021-06-03 21:51:23 -0300

    Update soma

--- soma.c
+++ soma.c
@@ -3,7 +3,7 @@
 int main (){
 
 int num1, num2;
-int soma = num1 + num2;
+int soma;
 
 
 
@@ -11,6 +11,8 @@ printf("Digite o primeiro número: \n");
 scanf("%d", &num1);
 printf("Digite o segundo número: \n");
 scanf("%d", &num2);
+
+soma = num1 + num2;
 printf("A soma dos dois númeos é: %d\n", soma);
 
 
